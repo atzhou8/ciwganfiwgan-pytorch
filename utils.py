16ff0b22e360a2058f566d1785bb33efff31e979
Andrej Leban <andrejleban@gmail.com>
2022-06-12 10:27:49 -0700

    Minor fix to continuation parsing

--- utils.py
+++ utils.py
@@ -3,8 +3,6 @@ import re
 
 
 def get_continuation_fname(CONT, logdir):
-    epoch = int(CONT)
-
     if CONT.lower() == "last":
         # Take last
         files = [f for f in os.listdir(logdir) if os.path.isfile(os.path.join(logdir, f))]
@@ -20,11 +18,14 @@ def get_continuation_fname(CONT, logdir):
         epoch = int(maxEpoch)
 
     else:
+
         # parametrized by the epoch
         fPrefix = f'epoch{CONT}_step'
         files = [f for f in os.listdir(logdir) if os.path.isfile(os.path.join(logdir, f))]
         fnames = [re.match(f"({re.escape(fPrefix)}\d+).*\.pt$", f) for f in files]
         # Take first if multiple matches (unlikely)
         fname = ([f for f in fnames if f is not None][0]).group(1)
+
+        epoch = int(CONT)
 
     return fname, epoch
