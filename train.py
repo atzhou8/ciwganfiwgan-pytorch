ce7a58177fcbfe4eb75ed57286dca31394961c6f
Andrej Leban <andrejleban@gmail.com>
2022-06-22 10:07:06 -0700

    added num categories to message

--- train.py
+++ train.py
@@ -171,11 +171,11 @@ if __name__ == "__main__":
         if train_Q:
             Q = WaveGANQNetwork(slice_len=SLICE_LEN, num_categ=NUM_CATEG).to(device).train()
         if args.fiw:
-            print("Training a fiwGAN")
+            print("Training a fiwGAN with ", NUM_CATEG, " categories.")
             optimizer_Q = optim.RMSprop(Q.parameters(), lr=LEARNING_RATE)
             criterion_Q = torch.nn.BCEWithLogitsLoss()
         elif args.ciw:
-            print("Training a ciwGAN")
+            print("Training a ciwGAN with ", NUM_CATEG, " categories.")
             optimizer_Q = optim.RMSprop(Q.parameters(), lr=LEARNING_RATE)
             criterion_Q = torch.nn.CrossEntropyLoss()
 
